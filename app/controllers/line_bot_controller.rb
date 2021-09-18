85060f6de5de3d9a698574cc8aaeadf7e6963f0f
TakumaShiga <tak07sweetvo@gmail.com>
2021-09-18 11:33:47 +0900

    Line::Bot::Clientクラスをインスタンス化

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -3,4 +3,13 @@ class LineBotController < ApplicationController
 
   def callback
   end
+
+  private
+
+  def client
+    @client ||= LINE::Bot::Client { |config| 
+    config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+    config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+    }
+  end
 end
