d36a01ebc40ee15f77cc7e980903c89e1d904076
TakumaShiga <tak07sweetvo@gmail.com>
2021-09-19 15:33:28 +0900

    メッセージボディの取得

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -2,14 +2,17 @@ class LineBotController < ApplicationController
   protect_from_forgery except: [:callback]
 
   def callback
+    body = request.body.read
   end
 
   private
 
   def client
-    @client ||= LINE::Bot::Client { |config| 
+    @client ||= LINE::Bot::Client.new { |config| 
     config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
     config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
     }
   end
 end
+
+
